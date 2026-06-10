26e4e1d8fdfb6ccbd28832ea7fb1e09b62fdfa43
MartinOjer <43086712@ifts24.edu.ar>
2026-06-09 22:10:36 -0300

    actualizar cors para vercel

--- backend/src/index.js
+++ backend/src/index.js
@@ -42,7 +42,10 @@ const PORT = process.env.PORT || 3001;
 // Middleware CORS: Permite que aplicaciones de otros dominios 
 // accedan a nuestra API (importante para frontends en React, Vue, etc.)
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: [
+    'http://localhost:5173',
+    'https://pre-entrega-proyecto-final-back-end.vercel.app'
+  ],
   credentials: true
 }));
 
